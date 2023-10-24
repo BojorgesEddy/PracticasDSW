d93b3c53bf6edc875e08f9c11c00c6549dbbc4e6
BojorgesEddy <bojorges.eddy@uabc.edu.mx>
2023-10-23 20:25:53 -0700

    Practica usando For, While, DoWhile

---
+++ pract28/pract28.js
@@ -0,0 +1,51 @@
+/*
+var diasSemana = ["lunes","martes","miercoles","jueves","viernes","sabado"];
+var concat = "";
+for (var i = 0; i < diasSemana.length; i++) {
+    concat = concat + diasSemana[i] +  " ";
+}
+alert(concat);
+
+let dia = prompt("Ingrese un dia.. \n");
+diasSemana.push(dia);
+concat = "";
+for (var i = 0; i < diasSemana.length; i++) {
+    concat = concat + diasSemana[i] +  " ";
+}
+alert(concat);
+
+let eliminar = parseInt(prompt("Elimnar de la lista... \n"));
+diasSemana.pop();
+concat = "";
+for (var i = 0; i < diasSemana.length; i++) {
+    concat = concat + diasSemana[i] +  " ";
+}
+alert(concat);
+*/
+/*---------------------------------------------------------------*/
+/*
+let conteo = 0;
+let ingresarNum;
+while(conteo < 15){
+    ingresarNum = prompt("Ingresar un numero.. \n");
+    conteo = conteo + parseInt(ingresarNum);
+    alert(conteo);
+
+    if (conteo > 15) {
+        alert("La suma tiene que dar 15 y dio "+conteo);
+        conteo = conteo - parseInt(ingresarNum);  
+    }
+}
+alert("La suma dio 15, terminado!!");
+*/
+/*---------------------------------------------------------------*/
+/*
+let num = 5;
+let factorial = 1;
+do {
+    factorial = num * factorial;
+    num--;
+    console.log(factorial);
+} while (factorial != 120); 
+*/
+/*---------------------------------------------------------------*/
