22df556a4999d5a5352bd07baca51e6fb13e8742
BojorgesEddy <bojorges.eddy@uabc.edu.mx>
2023-10-23 20:30:12 -0700

    Par o Impar, Alerta de Huracan, Calculadora

---
+++ pract30/pract30.js
@@ -0,0 +1,60 @@
+/*
+var num = parseInt(prompt("Ingrese un numero... "));
+function parImpar(){
+    var res = num%2;
+    if(res == 0){
+        alert("El numero "+num+" es par");
+    }else{
+        alert("El numero "+num+" es impar");
+    }
+}
+parImpar();
+*/
+
+/*
+var huracan = true;
+function aviso_huracan(hayUnHuracan){
+
+    var opcion = hayUnHuracan
+
+    switch (opcion) {
+        case true:
+            alert("Hay un Huracan");
+            break;
+        case false:
+            alert("No hay un Huracan");
+            break;
+        default:
+            alert("Ingrese un numero correcto");
+            break;
+    }
+}
+aviso_huracan(huracan);
+*/
+
+/*
+var ope,num1,num2;
+num1 = parseInt(prompt("Ingrese el primer numero"));
+num2 = parseInt(prompt("Ingrese el segundo numero"));
+ope = prompt("Que desea hacer: + - * / %")
+
+function OperacionesMatematicas(operacion,a,b){
+    switch (operacion) {
+        case "+":
+            return ("La suma es "+(a+b))
+        case "-":
+            return ("la resta es: "+(a-b));
+        case "*":
+            return ("la multiplicacion es: "+(a*b));
+        case "/":
+            return ("la divicion es: "+(a/b));
+        case "%":
+            return ("El residuo es: "+(a%b));
+        default:
+            return 0;
+    }
+}
+alert(OperacionesMatematicas(ope,num1,num2));
+*/
+
+
